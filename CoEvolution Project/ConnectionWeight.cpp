5e360011de23bace5c08912241272382a36a7b38
James Copping <james.copping97@gmail.com>
2018-11-14 11:09:14 +0000

    Removed first neural network approach and try for redesign in sperate project

--- CoEvolution Project/ConnectionWeight.cpp
+++
@@ -1,6 +0,0 @@
-#include "ConnectionWeight.h"
-
-ConnectionWeight::ConnectionWeight(Node * next, Node * prev, float weight): _weight(weight) , _next(next) , _prev(prev)
-{
-
-}
--- CoEvolution Project/main.cpp
+++ CoEvolution Project/main.cpp
@@ -2,37 +2,9 @@
 #include <vector>
 #include "Game.h"
 #include "DEFINITIONS.h"
-#include "GeneticNN.h"
-#include "GeneticAlgorithm.h"
-
 
 int main() {
-	std::vector<int> layers = { 2, 4, 5, 2 };
 
-	std::vector<GeneticNN> networks = std::vector<GeneticNN>();
-	Population<float> initPopulation = Population<float>();
-	for (int i = 0; i < 10; i++) {
-		networks.push_back(GeneticNN("Resources/neural_network/test" + std::to_string(i) + ".txt"));
-		initPopulation.push_back(GeneticNN::ConvertConnectionNetworkToChromosome(&(networks.at(i))));
-	}
-
-	GeneticAlgorithm<float> ga(initPopulation, 0.7f, 0.09f);
-
-	for (int i = 0; i < (int)networks.size(); i++) {
-		networks[i].LoadConnectionNetworkFromChromosome(ga.GetPopulation().at(i));
-	}
-
-	std::vector<float> inputs = { 0.2f, 0.4f };
-
-	for (GeneticNN& network : networks) {
-
-		for (float out : network.Update(inputs)) {
-			std::cout << out;
-		}
-	}
-
-	system("pause");
-
-	//Game(SCREEN_WIDTH, SCREEN_HEIGHT, "CoEvil Prototype");
+	Game(SCREEN_WIDTH, SCREEN_HEIGHT, "CoEvil Prototype");
 	return EXIT_SUCCESS;
 }
